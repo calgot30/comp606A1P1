ae2ad4d20f751bfc5b248f0e741cef6518de9434
callum gott <callumgott@gmail.com>
2019-08-06 16:24:18 +1200

    first addition of code to the files

---
+++ login.php
@@ -0,0 +1,12 @@
+<html>
+<h3>Please enter your details</h3>
+
+<form action=>
+Username:<br>
+<input type="text" name="username"> <br>
+Password<br>
+<input type="password" name="password">
+
+</form>
+
+</html>
